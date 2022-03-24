43496b5862fb9dbfdd31ba1faafafbd18d4aa16c
SEOINJAE <69412493+SEOINJAE@users.noreply.github.com>
2022-03-24 10:44:56 +0900

    1-3 반복문 이진 탐색 Done

--- 5.Binary Search/1-3. 반복문 이진탐색.py
+++ 5.Binary Search/1-3. 반복문 이진탐색.py
@@ -20,15 +20,15 @@ def binary_search(array, target, start, end):
         mid = (start + end) // 2
 
         # 찾은 경우 중간점 인덱스 반환
-            if array[mid] == target:
-                return mid
-            # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
-            elif array[mid] > target:
-                end = mid - 1
-            # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
-            else:
-                start = mid + 1
-        return None
+        if array[mid] == target:
+            return mid
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+        elif array[mid] > target:
+            end = mid - 1
+        # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+        else:
+            start = mid + 1
+    return None
 
 # n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
 n, target = list(map(int, input().split()))
