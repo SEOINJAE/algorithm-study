4243bbb371136eb0c4105f137e1b519019fcc2fe
SEOINJAE <69412493+SEOINJAE@users.noreply.github.com>
2022-05-20 16:52:37 +0900

    Create practice_note.py

---
+++ 99.Practice/practice_note.py
@@ -0,0 +1,36 @@
+# 10의 9제곱 : 0이 9개
+a = 1e9
+print(a)
+
+b= 2e3
+print(b)
+
+# 3.954
+c = 3594e-3
+print(c)
+
+
+a = 0.3 + 0.6
+print(a)
+
+if a == 0.9:
+    print(True)
+else:
+    print(False)
+
+
+a = 0.3 + 0.6
+print(a)
+
+if round(a, 4) == 0.9:
+    print(True)
+else:
+    print(False)
+
+
+print(round(a, 4))
+
+
+
+
+
