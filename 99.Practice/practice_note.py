35d2dd96cd409cca16a47510d96a2370df981b64
SEOINJAE <69412493+SEOINJAE@users.noreply.github.com>
2022-05-25 15:06:25 +0900

    Update practice_note.py

--- 99.Practice/practice_note.py
+++ 99.Practice/practice_note.py
@@ -31,6 +31,94 @@ else:
 print(round(a, 4))
 
 
+a = list()
+print(a)
+
+b = []
+print(b)
+
+#  크기가 N이고, 모든 값이 0인 1차원 리스트 초기화
+n = 10
+a = [0] * n
+print(a)
+
+print(a[3])
 
 
+# 0부터 19까지의 수 중에서 홀수만 포함하는 리스트
+array = [i for i in range(20) if i % 2 == 1]
+print(array)
+
+array = []
+for i in range(20):
+    if i % 2 == 1:
+        array.append(i)
+
+print(array)
+
+
+# 1부터 9까지의 수의 제곱 값을 포함하는 리스트
+array = [i * i for i in range(1, 10)]
+print(array)
+
+# N X M 크기의 2차원 리스트 초기화
+n = 3
+m = 4
+array = [[0] * m for _ in range(n)]
+print(array)
+
+array[1][1] = 5
+print(array)
+
+summary = 0
+for i in range(1, 10):
+    summary += i
+print(summary)
+
+for _ in range(5):
+    print("Hello World")
+
+
+# N X M 크기의 2차원 리스트 초기화(잘못된 방법)
+n = 3
+m = 4
+
+array = [[0] * m] * n
+print(array)
+
+
+array[1][1] = 5
+print(array)
+
+
+a = [1, 4, 3]
+print("기본 리스트:", a)
+
+a.append(2)
+print("삽입:", a)
+
+a.sort()
+print("오름차순 정렬:", a)
+
+a.sort(reverse= True)
+print("내림차순 정렬:", a)
+
+a.reverse()
+print("원소 뒤집기:", a)
+
+a.insert(2, 3)
+print("인덱스 2에 3 추가:", a)
+
+print("값이 3인 데이터 개수:", a.count(3))
+
+
+a.remove(1)
+print("값이 1인 데이터 삭제:", a)
+
+
+a = [1, 2, 3, 4, 5, 5]
+remove_set = {3, 5}
+
+result = [i for i in a if i not in remove_set]
+print(result)
 
